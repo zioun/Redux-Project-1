b8f4f36f185bf159f574a7c279788464849c3595
zioun <jionkhan0@gmail.com>
2025-01-24 10:29:57 +0600

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import CounterView from './features/counter/CounterView';
+
 
 function App() {
   return (
     <div className="App">
       <h1>React Project</h1>
+      <CounterView></CounterView>
     </div>
   );
 }
